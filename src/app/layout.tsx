12a2d321ec0116c177f7769c83db967eaee3e8be
Judson Ciribelli <judson.ciribelli@discente.ufma.br>
2025-02-24 18:38:40 -0300

    feat: add initial cart context and cart sheet component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 
+import { CartProvider } from "./[slug]/menu/context/cart";
+
 const poppins = Poppins({
   subsets: ["latin"],
   weight: ["200", "300", "400", "500", "600", "700", "800", "900"],
@@ -20,7 +22,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${poppins.className} antialiased`}>{children}</body>
+      <body className={`${poppins.className} antialiased`}>
+       <CartProvider>{children}</CartProvider> 
+      </body>
     </html>
   );
 }

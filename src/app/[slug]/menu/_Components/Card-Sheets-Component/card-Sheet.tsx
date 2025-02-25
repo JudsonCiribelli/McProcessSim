7f2c43db58e8bb9cd3211bdb923a4c4c89318df2
Judson Ciribelli <judson.ciribelli@discente.ufma.br>
2025-02-25 13:09:24 -0300

    feat: add initial cart item component

--- src/app/[slug]/menu/_Components/Card-Sheets-Component/card-Sheet.tsx
+++ src/app/[slug]/menu/_Components/Card-Sheets-Component/card-Sheet.tsx
@@ -1,24 +1,23 @@
 import { useContext } from "react";
 
-import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle } from "@/components/ui/sheet";
+import { Sheet, SheetContent,SheetHeader, SheetTitle } from "@/components/ui/sheet";
 
 import { CartContext } from "../../context/cart";
+import CartProductItem from "../Cart-Item-Component/cart-product-Item";
 
 const CartSheet = () => {
   const {isOpen, toggleCart, products} = useContext(CartContext)
   return ( 
     <Sheet open={isOpen} onOpenChange={toggleCart}>
-      <SheetContent>
+      <SheetContent className="w-[80%]">
         <SheetHeader>
-         <SheetTitle>Are you absolutely sure?</SheetTitle>
-          <SheetDescription>
-           This action cannot be undone. This will permanently delete your account
-           and remove your data from our servers.
-          </SheetDescription>
+         <SheetTitle className="text-left">Sacola</SheetTitle>
         </SheetHeader>
-        {products.map((product) => (
-          <h1 key={product.id}>{product.name} - {product.quantity}</h1>
-        ))}
+        <div className="py-5">
+         {products.map((product) => (
+          <CartProductItem product={product} key={product.id}/>
+         ))}
+        </div>
       </SheetContent>
     </Sheet>    
    );

83c1da28df7acf6742d72fe54258a1a8e28715e3
Judson Ciribelli <judson.ciribelli@discente.ufma.br>
2025-02-26 11:12:40 -0300

    feat: ensure order can be created

--- src/app/[slug]/menu/_Components/Products-List-Component/products.tsx
+++ src/app/[slug]/menu/_Components/Products-List-Component/products.tsx
@@ -1,39 +1,48 @@
 import { Product } from "@prisma/client";
 import Image from "next/image";
 import Link from "next/link";
-import { useParams } from "next/navigation";
+import { useParams, useSearchParams } from "next/navigation";
 
 import { FormatCurrency } from "@/helpers/format-currency";
 
-interface ProductsProps{
-  products: Product[]
+interface ProductsProps {
+  products: Product[];
 }
-const Products = ({products}: ProductsProps) => {
-  const {slug} = useParams<{slug: string}>()
-
-  return ( 
+const Products = ({ products }: ProductsProps) => {
+  const { slug } = useParams<{ slug: string }>();
+  const searchParams = useSearchParams();
+  const consumptionMethod = searchParams.get("consumptionMethod");
+  return (
     <div className="space-y-3 px-1.5">
       {products.map((product) => (
-        <Link href={`/${slug}/menu/${product.id}`} key={product.id} className="flex items-center justify-between gap-10 py-3 px-3 border-b">
+        <Link
+          href={`/${slug}/menu/${product.id}?consumptionMethod=${consumptionMethod}`}
+          key={product.id}
+          className="flex items-center justify-between gap-10 border-b px-3 py-3"
+        >
           {/* ESQUERDA */}
           <div>
             <h3 className="text-sm font-medium">{product.name}</h3>
-            <p className="line-clamp-2 text-sm text-muted-foreground">{product.description}</p>
-            <p className="pt-3 text-sm font-semibold">{FormatCurrency(product.price)}</p>
+            <p className="line-clamp-2 text-sm text-muted-foreground">
+              {product.description}
+            </p>
+            <p className="pt-3 text-sm font-semibold">
+              {FormatCurrency(product.price)}
+            </p>
           </div>
           {/* DIREITA */}
           <div className="relative min-h-[82px] min-w-[120px]">
             <Image
-            src={product.imageUrl}
-            fill
-            alt={product.name}
-            className="roudend-lg object-contain"
+              src={product.imageUrl}
+              fill
+              alt={product.name}
+              className="roudend-lg object-contain"
             />
           </div>
         </Link>
       ))}
     </div>
-   );
-}
- 
+  );
+};
+
 export default Products;
---
+++ src/app/[slug]/menu/actions/create-order.ts
@@ -0,0 +1,61 @@
+"use server";
+
+import { ConsumptionMethod } from "@prisma/client";
+
+import { db } from "@/lib/prisma";
+
+import { removeCpfPunctuation } from "../helpers/cpf";
+
+interface createOrderInput {
+  customerName: string;
+  customerCPF: string;
+  products: Array<{
+    id: string;
+    quantity: number;
+  }>;
+  consumptionMethod: ConsumptionMethod;
+  slug: string;
+}
+
+export const createOrder = async (Input: createOrderInput) => {
+  const restaurant = await db.restaurant.findUnique({
+    where: {
+      slug: Input.slug,
+    },
+  });
+  if (!restaurant) {
+    throw new Error("Restaurant not found");
+  }
+  const productsWithPrices = await db.product.findMany({
+    where: {
+      id: {
+        in: Input.products.map((product) => product.id),
+      },
+    },
+  });
+
+  const productsWithPricesAndQuantity = Input.products.map((product) => ({
+    productId: product.id,
+    quantity: product.quantity,
+    price: productsWithPrices.find((p) => p.id === product.id)!.price,
+  }));
+
+  await db.order.create({
+    data: {
+      status: "PENDING",
+      customerName: Input.customerName,
+      customerCPF: removeCpfPunctuation(Input.customerCPF),
+      orderProducts: {
+        createMany: {
+          data: productsWithPricesAndQuantity,
+        },
+      },
+      total: productsWithPricesAndQuantity.reduce(
+        (acc, product) => acc + product.price * product.quantity,
+        0,
+      ),
+      consumptionMethod: Input.consumptionMethod,
+      restaurantId: restaurant.id,
+    },
+  });
+};

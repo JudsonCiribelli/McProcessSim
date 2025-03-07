1690756477460e0183936bf4d75506654127d420
Judson Ciribelli <judson.ciribelli@discente.ufma.br>
2025-03-07 19:52:47 -0300

    feat: add stripe and  and webhooks integration

--- src/app/[slug]/menu/_Components/Finish-Order-Dialog-Component/finish-Order-Dialog.tsx
+++ src/app/[slug]/menu/_Components/Finish-Order-Dialog-Component/finish-Order-Dialog.tsx
@@ -1,12 +1,12 @@
 "use client";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { ConsumptionMethod } from "@prisma/client";
+import { loadStripe } from "@stripe/stripe-js";
 import { Loader2Icon } from "lucide-react";
 import { useParams, useSearchParams } from "next/navigation";
-import { useContext, useTransition } from "react";
+import { useContext, useState } from "react";
 import { useForm } from "react-hook-form";
 import { PatternFormat } from "react-number-format";
-import { toast } from "sonner";
 import { z } from "zod";
 
 import { Button } from "@/components/ui/button";
@@ -31,6 +31,7 @@ import {
 import { Input } from "@/components/ui/input";
 
 import { createOrder } from "../../actions/create-order";
+import { createStripeCheckout } from "../../actions/create-stripe-checkout";
 import { CartContext } from "../../context/cart";
 import { isValidCpf } from "../../helpers/cpf";
 
@@ -60,7 +61,7 @@ const FinishOrderDialog = ({ open, onOpenChange }: FinishOrderDialogProps) => {
   const { slug } = useParams<{ slug: string }>();
   const { products } = useContext(CartContext);
   const searchParams = useSearchParams();
-  const [isPending, startTransition] = useTransition()
+  const [isLoading, setIsLoading] = useState(false);
   const form = useForm<FormSchema>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -72,23 +73,37 @@ const FinishOrderDialog = ({ open, onOpenChange }: FinishOrderDialogProps) => {
 
   const onSubmit = async (data: FormSchema) => {
     try {
+      setIsLoading(true);
       const consumptionMethod = searchParams.get(
         "consumptionMethod",
       ) as ConsumptionMethod;
-      startTransition( async () =>{
-        await createOrder({
-          consumptionMethod,
-          customerCPF: data.cpf,
-          customerName: data.name,
-          products,
-          slug,
-        });
-        onOpenChange(false);
-        toast.success("Pedido finalizado com sucesso!")
-      })
 
+      const order = await createOrder({
+        consumptionMethod,
+        customerCPF: data.cpf,
+        customerName: data.name,
+        products,
+        slug,
+      });
+
+      const { sessionId } = await createStripeCheckout({
+        products,
+        orderId: order.id,
+        slug,
+        consumptionMethod,
+        cpf: data.cpf,
+      });
+      if (!process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY) return;
+      const stripe = await loadStripe(
+        process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY,
+      );
+      stripe?.redirectToCheckout({
+        sessionId: sessionId,
+      });
     } catch (error) {
       console.error(error);
+    } finally {
+      setIsLoading(false);
     }
   };
 
@@ -138,8 +153,12 @@ const FinishOrderDialog = ({ open, onOpenChange }: FinishOrderDialogProps) => {
                 )}
               />
               <DrawerFooter>
-                <Button variant="destructive" className="rounded-full" disabled={isPending}>
-                  {isPending &&  <Loader2Icon className="animate-spin"/> }
+                <Button
+                  variant="destructive"
+                  className="rounded-full"
+                  disabled={isLoading}
+                >
+                  {isLoading && <Loader2Icon className="animate-spin" />}
                   Finalizar
                 </Button>
                 <DrawerClose asChild>
